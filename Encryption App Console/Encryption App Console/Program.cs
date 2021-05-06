257df5e19a01c75303665469fa0eac368c599e8e
47ma <83763197+47ma@users.noreply.github.com>
2021-05-06 13:24:31 +0200

    Add files via upload

---
+++ Encryption App Console/Encryption App Console/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Encryption_App_Console
+{
+    class Program
+    {
+        static int Main()
+        {
+            Console.WriteLine(" Enter Your Key only 10 digit allowed");
+            string Key = Console.ReadLine();
+            for (int y = 0; y < Key.Length; y++)
+                if (Key[y] == Key[y + 1])
+                {
+                    Console.WriteLine("\n Error wrong key:Key can't be duplicate number");
+                    return 0;
+                }
+                else
+                
+                    for (int x = 0; x < 10; x++)
+                        if (char.IsDigit(Key[x]))
+                            if (Key.Length == 10)
+                            {
+                                Console.WriteLine("\n Enter Your message");
+                                string Message = Console.ReadLine();
+                                if (char.IsDigit(Message[x]))
+                                {
+                                    Console.WriteLine("\n Encrypted Message is: ");
+                                    for (int i = 0; i < Message.Length; i++)
+                                    {
+                                        for (int j = 0; j < Key.Length; j++)
+                                        {
+                                            if (Message[i] == Key[j])
+                                                Console.Write(j);
+                                        }
+                                    }
+                                    Console.WriteLine("\n Key is: \n " + Key);
+                                    break;
+                                } 
+                                    
+                            else
+                            {
+                                Console.WriteLine("\n Message must be digits");
+                                break;
+                            }
+                            }
+                            else
+                            {
+                                Console.WriteLine("\n Key must be 10 digits");
+                                break;
+                            }
+                        else
+                        {
+                            Console.WriteLine("\n Key must be digits ");
+                            break;
+                        } 
+            return 0 ;
+                }               
+
+        
+    }
+}
---
+++ Encryption App Windows form/Encryption App Windows form/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encryption_App_Windows_form
+{
+    public partial class Encryption : Form
+    {
+        public Encryption()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 10)
+            {
+                string Key = textBox1.Text;
+                string Message = textBox2.Text;
+                string Encryption = null;   
+                for (int i = 0; i < Message.Length; i++)
+                {
+                    for (int j = 0; j < Key.Length; j++)
+                    {
+                        if (Message[i] == Key[j])
+                            Encryption += (j).ToString() ;    
+                    }
+                }
+                textBox3.Text = Encryption;
+            }
+            else
+            {
+                MessageBox.Show("Error key must be 10 digits");
+            }
+        }
+            private void Encryption_Load(object sender, EventArgs e)
+            {
+
+            }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Only Digits");
+            }
+        }
+
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Only Digits");
+            }
+        }
+    }
+    }
